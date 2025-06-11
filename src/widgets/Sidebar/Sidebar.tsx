d74dfe57c36b6c65fd8ff9f8ed34589f99fbd0d1
KArtem <k6art5@gmail.com>
2025-06-11 16:37:30 +0700

    create Sidebar widget

---
+++ src/widgets/Sidebar/Sidebar.tsx
@@ -0,0 +1,29 @@
+import { NavLink } from 'react-router-dom'
+import classNames from 'classnames/bind'
+
+import { routes } from '../../app/routes'
+import { Icon } from '../../shared'
+
+import styles from './Sidebar.module.scss'
+
+const cx = classNames.bind(styles)
+
+export const Sidebar = () => {
+  return (
+    <nav className={cx('sidebar')}>
+      <h3 className={cx('sidebar__title')}>Карты оплаты</h3>
+      <ul className={cx('sidebar__list')}>
+        {routes.map((route) => (
+          <NavLink
+            key={route.path}
+            to={route.path}
+            className={({ isActive }) => cx('sidebar__list__item', { 'active': isActive })}
+          >
+            {route.icon && <Icon name={route.icon} size='medium'/>}
+            <p className={cx('p-small')}>{route.title}</p>
+          </NavLink>
+        ))}
+      </ul>
+    </nav>
+  )
+}
