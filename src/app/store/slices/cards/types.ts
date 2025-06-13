2e564b711518051210456c1deca6cb2968e01c0d
KArtem <k6art5@gmail.com>
2025-06-13 16:37:03 +0700

    correct type ICards by ICard from widget table

--- src/app/store/slices/cards/types.ts
+++ src/app/store/slices/cards/types.ts
@@ -1,15 +1,5 @@
+import { ICard } from '../../../../widgets/Table/type'
+
 export interface ICards {
-  data: [
-    {
-      id: number,
-      ident_num: string,
-      status_id: number,
-      status_name: string,
-      nominal: number,
-      dealer_id: number,
-      dealer_name: string,
-      secret_code: string,
-      created_dt: string
-    }
-  ]
+  data: ICard[]
 }
