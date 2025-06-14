b53d46d8309000723fa2e63025126bab7a5420af
kartem <k6art5@gmail.com>
2025-06-14 10:59:47 +0700

    added Wrapper widget in Dashboard page

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -6,7 +6,7 @@ import { ModalContext } from'../../app/contexts'
 import { ModalContextTypes } from '../../app/providers/types'
 import { getCardsData } from '../../app/api'
 import { setCardsData } from '../../app/store/slices'
-import { Sidebar, Header, Table } from '../../widgets'
+import { Wrapper, Table } from '../../widgets'
 import { Pagination } from '../../feature'
 import { useDispatch, useSelector } from '../../hooks'
 import { Button } from '../../shared'
@@ -15,7 +15,7 @@ import styles from './Dashboard.module.scss'
 
 const cx = classNames.bind(styles)
 
-export const Dashboard = () => {
+const Page = () => {
   const [searchParams, setSearchParams] = useSearchParams()
   const { openHandler: openHandlerModal } = useContext<ModalContextTypes>(ModalContext)
   const { data: cardsData } = useSelector((state) => state.cardsData)
@@ -63,38 +63,40 @@ export const Dashboard = () => {
   }, [page])
 
   return (
-    <>
-      <Sidebar />
-      <div className={cx('wrapper')}>
-        <Header title='Главная' />
-        <main className={cx('main')}>
-          <div className={cx('search-wrapper')}>
-            <h3>Search component</h3>
-          </div>
-          <div className={cx('functions-button')}>
-            <div className={cx('functions-button__left')}>
-              <Button type='green' onClick={activationHandler}>Активировать ✅</Button>
-              <Button type='gray' onClick={deActivationHandler}>Деактивировать ❌</Button>
-            </div>
-            <div className={cx('functions-button__right')}>
-              <Button className={cx('functions-button__generate-excel')} type='orange' onClick={generateExcelHandler}>Excel</Button>
-              <Button className={cx('functions-button__create-card')} type='green' onClick={openModalCreateCardHandler}>Создать карты</Button>
-              <Button onClick={openModalCreateCardHandler}>Штрихкод</Button>
-            </div>
-          </div>
-          <hr/>
-          <Table data={cardsData} className={cx('table')}/>
-          <hr/>
-          <Pagination
-            itemsPerPage={itemsPerPage}
-            page={page}
-            setPage={setPage}
-            changePageHandler={changePageHandler}
-            totalPages={totalPages}
-            totalItems={totalItems}
-          />
-        </main>
+      <>
+      <div className={cx('search-wrapper')}>
+        <h3>Search component</h3>
       </div>
-    </>
+      <div className={cx('functions-button')}>
+        <div className={cx('functions-button__left')}>
+          <Button type='green' onClick={activationHandler}>Активировать ✅</Button>
+          <Button type='gray' onClick={deActivationHandler}>Деактивировать ❌</Button>
+        </div>
+        <div className={cx('functions-button__right')}>
+          <Button className={cx('functions-button__generate-excel')} type='orange' onClick={generateExcelHandler}>Excel</Button>
+          <Button className={cx('functions-button__create-card')} type='green' onClick={openModalCreateCardHandler}>Создать карты</Button>
+          <Button onClick={openModalCreateCardHandler}>Штрихкод</Button>
+        </div>
+      </div>
+      <hr/>
+      <Table data={cardsData} className={cx('table')}/>
+      <hr/>
+      <Pagination
+        itemsPerPage={itemsPerPage}
+        page={page}
+        setPage={setPage}
+        changePageHandler={changePageHandler}
+        totalPages={totalPages}
+        totalItems={totalItems}
+      />
+      </>
+  )
+}
+
+export const Dashboard = () => {
+  return (
+    <Wrapper>
+      <Page />
+    </Wrapper>
   )
 }
