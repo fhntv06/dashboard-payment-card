57374594fb704aa7a5583d00ad332cab448a9ad0
kartem <k6art5@gmail.com>
2025-06-14 12:54:56 +0700

    correct modal CreateCard

--- src/shared/ui/CreateCardModal/CreateCardModal.tsx
+++ src/shared/ui/CreateCardModal/CreateCardModal.tsx
@@ -1,8 +1,10 @@
+import { useState } from 'react'
 import classNames from 'classnames/bind'
 
 import { Button, Select } from '../'
 
 import styles from './CreateCardModal.module.scss'
+import { Input } from '../Input'
 
 const cx = classNames.bind(styles)
 
@@ -37,28 +39,56 @@ const dealerData = [
 ]
 
 export const CreateCardModal = () => {
+  const [count, setCount] = useState<string>('')
+  const [nominal, setNominal] = useState<string>(nominalData[0].name)
+  const [dealer, setDealer] = useState<string>(dealerData[0].name)
+
   const createCardHandler = () => {
-    console.log('createCardHandler')  
+    alert(`Карты с номиналом ${nominal}, количеством ${count}, дилером ${dealer} созданы!`)
   }
+  const setCountHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCount(event.target.value)
+  }
+  const setNominalDataHandler = (nominal: string) => {
+    setNominal(nominal)
+  }
+    const setDealerDataHandler = (nominal: string) => {
+    setDealer(nominal)
+  }
+
+  // данные для дальнейшей обработки
+  console.log({ nominal, count, dealer })
 
   return (
     <>
       <h2 className={cx('modal__title')}>Создание карты</h2>
       <div className={cx('modal__content')}>
         <div className={cx('modal__content__column')}>
-          <p className={cx('p-medium')}>Номинал *</p>
-          <Select data={nominalData} />
+          <p className={cx('p-medium')}>Номинал *</p> 
+          <Select data={nominalData} setData={setNominalDataHandler} />
         </div>
         <div className={cx('modal__content__column')}>
           <p className={cx('p-medium')}>Количество *</p>
-          <input name='count' className={cx('input')} placeholder='Введите количество' />
+          <Input
+            name='count'
+            className={cx('input')}
+            placeholder='Введите количество'
+            value={count}
+            onChange={setCountHandler}
+          />
         </div>
         <div className={cx('modal__content__column')}>
           <p className={cx('p-medium')}>Дилер *</p>
-          <Select data={dealerData} />
+          <Select data={dealerData} setData={setDealerDataHandler} />
         </div>
       </div>
-      <Button type='green' onClick={createCardHandler}>Создать карты</Button>
+      <Button
+        type='green'
+        onClick={createCardHandler}
+        disabled={!count}
+      >
+        Создать карты
+      </Button>
     </>
   )
 }
