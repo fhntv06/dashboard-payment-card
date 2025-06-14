5c9234cfae25e22272b4b2701c173b2bb00fb140
kartem <k6art5@gmail.com>
2025-06-14 14:58:09 +0700

    create HiddenField feature

--- src/feature/HiddenField/HiddenField.tsx
+++ src/feature/HiddenField/HiddenField.tsx
@@ -1,40 +1,54 @@
 import { useState } from 'react'
+import {
+  Input,
+//  Icon
+} from '../../shared'
 import classNames from 'classnames/bind'
 
-import styles from './HiddenField.module.scss'
-// import {Icon} from "../../../../shared";
+import styles from './HiddenField.module.scss';
 
-const cx = classNames.bind(styles)
+const cx = classNames.bind(styles);
 
 interface IProps {
-  code: string
+  value: string
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
   className?: string
 }
 
-export const HiddenField = ({ code, className }: IProps) => {
-  const [isVisible, setVisible] = useState<boolean>(false)
+export const HiddenField = ({ value, onChange, className }: IProps) => {
+  const [isVisible, setIsVisible] = useState<boolean>(false)
 
-  const visibleHandler = () => {
-    setVisible((prevState) => !prevState)
+  const changeVisible = () => {
+    setIsVisible((prevState) => !prevState)
   }
 
   return (
-    <div className={cx('secret-code', className)} onClick={visibleHandler}>
-      <p className={cx('p-medium font-w-bold')}>{isVisible ? code : '*******************'}</p>
-      {isVisible ? (
-        // <Icon name='eye-close' size='big' />
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor">
-          <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-          <circle cx="12" cy="12" r="3"></circle>
-        </svg>
-      ) : (
-        // <Icon name='eye' size='big' />
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor">
-          <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-          <circle cx="12" cy="12" r="3"></circle>
-          <line x1="1" y1="1" x2="23" y2="23" strokeWidth="2"></line>
-        </svg>
-      )}
+    <div className={cx('input', className)}>
+      <Input
+        name='password'
+        type={isVisible ? 'text' : 'password'}
+        placeholder='Пароль'
+        value={value}
+        onChange={onChange}
+        typeStyle='black'
+      />
+      <button className={cx('visible-control')} onClick={changeVisible} type='button'>
+        {isVisible ? (
+          // <Icon name='eye-close' size='big' />
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="white" stroke="currentColor">
+            <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" fill="white"></path>
+            <circle cx="12" cy="12" r="3"></circle>
+          </svg>
+        ) : (
+          // <Icon name='eye' size='big' />
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="white" stroke="currentColor">
+            <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" fill="white"></path>
+            <circle cx="12" cy="12" r="3"></circle>
+            <line x1="1" y1="1" x2="23" y2="23" strokeWidth="2"></line>
+          </svg>
+        )}
+      </button>
     </div>
+
   )
 }
