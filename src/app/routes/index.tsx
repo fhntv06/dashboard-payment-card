d731cb3680153f494e901d6a37400693abf4c7cf
kartem <k6art5@gmail.com>
2025-06-15 12:39:51 +0700

    correct routes for privateRoutes

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,12 +1,12 @@
 import type { ComponentType } from 'react'
 
-import { Dashboard, Statistic, History, Role } from '../../pages'
+import { Dashboard, Statistic, History, Role, Auth, Card } from '../../pages'
 
 interface Route {
-  path: string;
-  Component: ComponentType;
-  title?: string;
-  icon?: string;
+  path: string
+  Component: ComponentType
+  title?: string
+  icon?: string
 }
 
 export const pathsRoutes: {
@@ -21,11 +21,20 @@ export const pathsRoutes: {
   statistic: `${import.meta.env.VITE_PUBLIC_PATH_FOR_GITHUB_PAGES}/statistic`,
   history: `${import.meta.env.VITE_PUBLIC_PATH_FOR_GITHUB_PAGES}/history`,
   role: `${import.meta.env.VITE_PUBLIC_PATH_FOR_GITHUB_PAGES}/role`,
-  card: `${import.meta.env.VITE_PUBLIC_PATH_FOR_GITHUB_PAGES}/card`,
+  card: `${import.meta.env.VITE_PUBLIC_PATH_FOR_GITHUB_PAGES}/dashboard/card/:id`,
   auth: `${import.meta.env.VITE_PUBLIC_PATH_FOR_GITHUB_PAGES}/auth`,
 }
 
-export const routes: Route[] = [
+export const privateRoutes: Route[] = [
+  { path: pathsRoutes.dashboard, Component: Dashboard, title: 'Главная' },
+  { path: pathsRoutes.statistic, Component: Statistic, title: 'Статистика' },
+  { path: pathsRoutes.history, Component: History, title: 'История действий' },
+  { path: pathsRoutes.role, Component: Role, title: 'Ролевая модель' },
+  { path: pathsRoutes.card, Component: Card, title: 'Карточка карты' },
+  { path: pathsRoutes.auth, Component: Auth, title: 'Авторизация' },
+]
+
+export const sideBarMenuRoutes: Route[] = [
   { path: pathsRoutes.dashboard, Component: Dashboard, title: 'Главная', icon: 'home' },
   { path: pathsRoutes.statistic, Component: Statistic, title: 'Статистика', icon: 'person' },
   { path: pathsRoutes.history, Component: History, title: 'История действий', icon: 'person' },
